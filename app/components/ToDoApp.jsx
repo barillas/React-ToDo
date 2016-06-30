98ee5ff25dfb3d090f79bce8ca5bd37d36a0dff0
Oscar Barillas <oscarbarillas93@gmail.com>
2016-06-30 12:28:56 -0700

    Add ToDo App Component

---
+++ app/components/ToDoApp.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function () {
+    return (
+
+      <div>
+        To Do App.JSX
+      </div>
+    )
+  }
+
+});
+
+module.exports = TodoApp;
