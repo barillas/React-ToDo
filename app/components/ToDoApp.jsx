af06eb778fc817dac843d8eb82e15decc6a2c2cc
Oscar Barillas <oscarbarillas93@gmail.com>
2016-06-30 18:23:31 -0700

    Get Initial State ids made

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -2,6 +2,25 @@ var React = require('react');
 
 var TodoApp = React.createClass({
   render: function () {
+    getInitialState: function() {
+      return {
+        todos: [
+          {
+            id: 1,
+            text: "Walk the dog"
+          }, {
+            id: 2,
+            text: "Clean the room"
+          }, {
+            id: 3,
+            text: "Eat Fried Chicken"
+          }
+          
+        ]
+
+        }
+
+    },
     return (
 
       <div>
