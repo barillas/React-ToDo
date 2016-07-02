853705f2107b8b3075abc5a3a290bee25276ae86
Oscar Barillas <oscarbarillas93@gmail.com>
2016-07-01 19:34:29 -0700

    Add to do app skeleton

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+
+    render: function() {
+
+        return (
+          <div>
+
+            <p>Add To do skeleton</p>
+
+          </div>
+
+        )
+    }
+
+})
+
+module.exports = AddTodo
