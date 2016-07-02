c38c1f4484e09f8f9a6ebf858f24c5a919d9a781
Oscar Barillas <oscarbarillas93@gmail.com>
2016-07-01 19:59:20 -0700

    Wrote tests for Add Todo

---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddTodo = require('AddTodo')
+
+describe('Add Todo', () => {
+  it('Should exist', () => {
+    expect(AddTodo).toExist();
+  });
+
+  it('should call onAddTodo if valid text entered', () => {
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo = {spy} />);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+    addTodo.refs.addTodo.value = "Hello";
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith("Hello");
+
+
+  });
+
+})
